a6f28efcd7b0f19cec7dcedca9e7c5dcae1e5f92
skuttenkuler <samkiddmusic@gmail.com>
2019-12-04 22:45:19 -0800

    set up and sudo code

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//pointers
+
+//timer
+//eventListener to start quiz
+    //when pushed, initialize timer
+    //initialize quiz function
+//eventListner to go to highscore page
+
+
+//function quiz
+    //set score variable
+    //timer == '0' return the value or score and stop quiz
+    //prompt user with question
+    //if correct or true then 
+        //score ++
+    //else
+        //don't add point
+
+//take score and add name to it
+//send data to storage
+
+
+
+//pull storage and display on highscore.html
