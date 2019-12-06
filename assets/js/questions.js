4bb87ebb2ece48f69cf07ccbe182e69f5df173ce
skuttenkuler <samkiddmusic@gmail.com>
2019-12-05 21:36:17 -0800

    questions and background

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+var questions = [
+    {
+      title: "Which town does the Simpsons take place? ",
+      choices: ["Nashville", "Springfield", "Springton", "Portland"],
+      answer: "Springfield"
+    },
+    {
+      title: "Which is NOT one of Bart's catchphrases?",
+      choices: ["Eat my shorts!", "Don't have a cow man", "Aye carumba", "Woohoo!"],
+      answer: "Woohoo!"
+    },
+    {
+        title: "Whoe runs the Kwik-E-Mart?",
+        choices: ["Apu", "Mr. Burns", "Barney", "Homer"],
+        answer: "Apu"
+    },
+    {
+        title: "What's the name of Mr. Burns bear",
+        choices: ["Coco", "Popo", "Bobo", "Slomo"],
+        answer: "Bobo"
+    },
+    {
+        title: "What did the Simpsons get for their first Christmas?",
+        choices: ["A car", "A grill", "A cat", "A dog"],
+        answer: "A dog"
+    },
+
+  ];
+
+  
